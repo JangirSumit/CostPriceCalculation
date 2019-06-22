c4dfe3dea8c0c5a8ae64af7ff269a159b2024eba
Sumit Jangir <sumitjangir.jecrc@gmail.com>
2019-06-22 20:09:12 +0530

    Add project files.

---
+++ CostPriceCalculation/FormCostPriceCalculation.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace CostPriceCalculation
+{
+    partial class FormCostPriceCalculation
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlInputDetails = new System.Windows.Forms.Panel();
+            this.pnlOutputDetails = new System.Windows.Forms.Panel();
+            this.lblSharesSold = new System.Windows.Forms.Label();
+            this.lblPricePerShare = new System.Windows.Forms.Label();
+            this.lblSellDate = new System.Windows.Forms.Label();
+            this.txtSharesSold = new System.Windows.Forms.TextBox();
+            this.txtPricePerShare = new System.Windows.Forms.TextBox();
+            this.txtSellDate = new System.Windows.Forms.TextBox();
+            this.lblDateFormate = new System.Windows.Forms.Label();
+            this.pnlInputDetails.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlInputDetails
+            // 
+            this.pnlInputDetails.BackColor = System.Drawing.Color.DimGray;
+            this.pnlInputDetails.Controls.Add(this.lblDateFormate);
+            this.pnlInputDetails.Controls.Add(this.txtSellDate);
+            this.pnlInputDetails.Controls.Add(this.txtPricePerShare);
+            this.pnlInputDetails.Controls.Add(this.txtSharesSold);
+            this.pnlInputDetails.Controls.Add(this.lblSellDate);
+            this.pnlInputDetails.Controls.Add(this.lblPricePerShare);
+            this.pnlInputDetails.Controls.Add(this.lblSharesSold);
+            this.pnlInputDetails.Location = new System.Drawing.Point(13, 13);
+            this.pnlInputDetails.Name = "pnlInputDetails";
+            this.pnlInputDetails.Size = new System.Drawing.Size(581, 199);
+            this.pnlInputDetails.TabIndex = 0;
+            // 
+            // pnlOutputDetails
+            // 
+            this.pnlOutputDetails.BackColor = System.Drawing.Color.DimGray;
+            this.pnlOutputDetails.Location = new System.Drawing.Point(12, 218);
+            this.pnlOutputDetails.Name = "pnlOutputDetails";
+            this.pnlOutputDetails.Size = new System.Drawing.Size(581, 219);
+            this.pnlOutputDetails.TabIndex = 1;
+            // 
+            // lblSharesSold
+            // 
+            this.lblSharesSold.AutoSize = true;
+            this.lblSharesSold.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSharesSold.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.lblSharesSold.Location = new System.Drawing.Point(91, 23);
+            this.lblSharesSold.Name = "lblSharesSold";
+            this.lblSharesSold.Size = new System.Drawing.Size(100, 18);
+            this.lblSharesSold.TabIndex = 0;
+            this.lblSharesSold.Text = "Shares Sold";
+            // 
+            // lblPricePerShare
+            // 
+            this.lblPricePerShare.AutoSize = true;
+            this.lblPricePerShare.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPricePerShare.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.lblPricePerShare.Location = new System.Drawing.Point(91, 83);
+            this.lblPricePerShare.Name = "lblPricePerShare";
+            this.lblPricePerShare.Size = new System.Drawing.Size(125, 18);
+            this.lblPricePerShare.TabIndex = 1;
+            this.lblPricePerShare.Text = "Price per Share";
+            // 
+            // lblSellDate
+            // 
+            this.lblSellDate.AutoSize = true;
+            this.lblSellDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSellDate.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.lblSellDate.Location = new System.Drawing.Point(91, 136);
+            this.lblSellDate.Name = "lblSellDate";
+            this.lblSellDate.Size = new System.Drawing.Size(76, 18);
+            this.lblSellDate.TabIndex = 2;
+            this.lblSellDate.Text = "Sell Date";
+            // 
+            // txtSharesSold
+            // 
+            this.txtSharesSold.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSharesSold.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.txtSharesSold.Location = new System.Drawing.Point(282, 23);
+            this.txtSharesSold.Name = "txtSharesSold";
+            this.txtSharesSold.Size = new System.Drawing.Size(200, 24);
+            this.txtSharesSold.TabIndex = 3;
+            // 
+            // txtPricePerShare
+            // 
+            this.txtPricePerShare.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPricePerShare.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.txtPricePerShare.Location = new System.Drawing.Point(282, 80);
+            this.txtPricePerShare.Name = "txtPricePerShare";
+            this.txtPricePerShare.Size = new System.Drawing.Size(200, 24);
+            this.txtPricePerShare.TabIndex = 4;
+            // 
+            // txtSellDate
+            // 
+            this.txtSellDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSellDate.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.txtSellDate.Location = new System.Drawing.Point(282, 133);
+            this.txtSellDate.Name = "txtSellDate";
+            this.txtSellDate.Size = new System.Drawing.Size(200, 24);
+            this.txtSellDate.TabIndex = 5;
+            // 
+            // lblDateFormate
+            // 
+            this.lblDateFormate.AutoSize = true;
+            this.lblDateFormate.ForeColor = System.Drawing.Color.Transparent;
+            this.lblDateFormate.Location = new System.Drawing.Point(378, 164);
+            this.lblDateFormate.Name = "lblDateFormate";
+            this.lblDateFormate.Size = new System.Drawing.Size(112, 13);
+            this.lblDateFormate.TabIndex = 7;
+            this.lblDateFormate.Text = "(Format : dd/mm/yyyy)";
+            // 
+            // FormCostPriceCalculation
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(606, 463);
+            this.Controls.Add(this.pnlOutputDetails);
+            this.Controls.Add(this.pnlInputDetails);
+            this.Name = "FormCostPriceCalculation";
+            this.Text = "Cost Price Calculation";
+            this.pnlInputDetails.ResumeLayout(false);
+            this.pnlInputDetails.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlInputDetails;
+        private System.Windows.Forms.Panel pnlOutputDetails;
+        private System.Windows.Forms.TextBox txtPricePerShare;
+        private System.Windows.Forms.TextBox txtSharesSold;
+        private System.Windows.Forms.Label lblSellDate;
+        private System.Windows.Forms.Label lblSharesSold;
+        private System.Windows.Forms.TextBox txtSellDate;
+        private System.Windows.Forms.Label lblPricePerShare;
+        private System.Windows.Forms.Label lblDateFormate;
+    }
+}
+
